e115cdff0db892c6505104de9359eb6fde53e28e
pedro <pedro@DESKTOP-R8CBM62>
2022-07-06 16:53:31 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pedro Henrique Romero Cedro || RA:4722002 
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i.ToString());
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
